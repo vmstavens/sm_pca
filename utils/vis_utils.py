a4900af5a374899011feb7f954d5dec32a909e0a
joste17 <joste17@student.sdu.dk>
2022-03-22 14:43:10 +0100

    Clustering report done

--- utils/vis_utils.py
+++ utils/vis_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
 
-def visualize_generic(data:np.ndarray, rows:int, cols:int, fig_title:str, sub_titles: List, img_name="img.png", save_img:bool = False) -> None:
+def visualize_generic(data:np.ndarray, rows:int, cols:int, fig_title:str, sub_titles:List=[], img_name="img.png", save_img:bool = False) -> None:
     """
     data : np.ndarray -> list of images or a single image
     rows: int -> the number of rows in the image showing matrix, if only one image rows = 1
@@ -10,7 +10,6 @@ def visualize_generic(data:np.ndarray, rows:int, cols:int, fig_title:str, sub_ti
     fig_title: str -> the title of the plot
     sub_titles: List -> list of sub plot titles, if only 1 image is given, this is ignored 
     """
-
     # print("data shape = ",data.shape)
     # print("test", int(np.sqrt(len(data[0]))))
 
@@ -28,16 +27,19 @@ def visualize_generic(data:np.ndarray, rows:int, cols:int, fig_title:str, sub_ti
 
     # multiple images
     # fig = plt.figure()
-    fig = plt.figure(figsize=(10, 5))
+    fig = plt.figure(figsize=(cols, rows+1))
     fig.suptitle(fig_title, fontsize=16, color="blue", fontweight="bold")
     number_of_img = data.shape[0]
     img_rows = int(np.sqrt(len(data[0])))
     img_cols = img_rows
     for i in range(1, number_of_img + 1):
         ax = fig.add_subplot(rows, cols, i)
-        ax.set_title(sub_titles[i-1], color="blue", fontweight="bold")
+        if any(sub_titles):
+            ax.set_title(sub_titles[i-1], color="blue", fontweight="bold")
+        ax.set_yticks([])
+        ax.set_xticks([])
         plt.imshow(data[i-1].reshape(img_rows, img_cols), cmap="gray")
-        plt.tight_layout()
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     if save_img == True:
         plt.savefig(img_name)
     plt.show()
